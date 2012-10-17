bd6ace010f1353fdec2284ead90028f8c9fa2d6b
Kyle <kyle@caitsith.(none)>
2012-10-17 16:33:39 -0400

    Finishing up implementation. KInda ugly, but seemingly works

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <string>
+#include <sstream>
+#include "processor.h"
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::string;
+using std::stringstream;
+
+int main(int argc, char*argv[]){
+	if(argc<2){
+		cout<<"Need to give filename as argument"<<endl;
+		exit(1);
+	}
+	string filename{argv[1]};
+	ifstream fr{filename};
+	int num_programs = 0;
+	string line = "";
+	getline(fr, line);
+	stringstream(line)>>num_programs;
+	//cout<<"num p: "<<num_programs<<endl;
+	getline(fr, line);
+	for(int i = 0; i<num_programs; i++){
+		Processor proc(10,1000,1000);
+		proc.load(fr);
+		cout<<proc.run()<<endl;
+	}
+	return 0;
+}
